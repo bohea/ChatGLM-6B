fe2d57910e5bfadd0facb90df5b16ff778dc937e
braces <libin375@163.com>
2023-04-26 12:23:09 +0800

    Update api.py
    add stream for api

--- api.py
+++ api.py
@@ -48,6 +48,33 @@ async def create_item(request: Request):
     torch_gc()
     return answer
 
+@app.post("/stream")
+async def create_item(request: Request):
+    global model, tokenizer
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    max_length = json_post_list.get('max_length')
+    top_p = json_post_list.get('top_p')
+    temperature = json_post_list.get('temperature')
+    response = model.stream_chat(tokenizer,
+                                   prompt,
+                                   history=history,
+                                   max_length=max_length if max_length else 2048,
+                                   top_p=top_p if top_p else 0.7,
+                                   temperature=temperature if temperature else 0.95)
+
+    def response_gen(res):
+        previous = ""
+        for content, _ in res:
+            chunk = content[len(previous):]
+            previous = content
+            yield chunk
+        torch_gc()
+    return StreamingResponse(response_gen(response), media_type='text/event-stream')
+
 
 if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
